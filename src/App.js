009baf0402d1dd6dcd006db5489fa0ae523380e6
Abhilashkumar <m.abhilash65@gmail.com>
2022-01-05 22:04:24 +0530

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,107 +1,162 @@
-import { useState } from "react"
-import { double } from"./Counter"
+import { useState, useEffect } from "react";
 import "./App.css";
-import { MovieList } from "./Movies/MovieList.1";
-import {Switch,Route,Link, Redirect} from "react-router-dom"
-import { INITIAL_MOVIES } from "./Movies/INITIAL_MOVIES";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import { MovieDetails } from "./Movies/MovieDetails";
-import { AddColor } from "./AddColor";
-import { AddMovie } from "./Movies/AddMovie";
-import { EditMovie } from "./Movies/EditMovie";
-// import { AddMovie } from "./Movies/AddMovie"
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import { MovieList } from "./MovieList";
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useHistory } from "react-router-dom"
-function App() {
-    const [movies, setMovies] = useState(INITIAL_MOVIES);
-    const history = useHistory();
-    return (
+import { Switch, Route, Redirect } from "react-router-dom";
+import { AddColor } from "./AddColor";
+import { useHistory } from "react-router-dom";
+import { AddMovie } from "./AddMovie";
+import { EditMovie } from "./EditMovie";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { MovieDetails } from "./MovieDetails";
+import { BasicForm } from "./BasicForm";
+
+export default function App() {
+  const [movies, setMovies] = useState([])
+  const history = useHistory();
+
+  const [mode, setMode] = useState("light");
+
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  })
+
+  useEffect(() => {
+    fetch('https://61d5b9cc2b4f730017a82a0e.mockapi.io/movies', {
+      method: "GET",
+    }) 
+    .then((data) => data.json())
+    .then((mvs) => setMovies(mvs))
+  }, [])
+
+  const paperStyles = {minHeight: '100vh'}
+
+  return (
+    <ThemeProvider theme={theme}>
+    <Paper elevation={3} style={paperStyles}>
+
     <div className="App">
-         {/* <li>
-          <Link to="/">Home</Link>
-          </li>
-          <li>
-          <Link to="/movies">Movie</Link>
-          </li>
-          <li>
-          <Link to="/movies/add">Add Movie</Link>
-          </li>
-          <li>
-          <Link to="/color-game">Color Game</Link>
-          </li> */}
-          <AppBar position="static">
+    <AppBar position="static">
         <Toolbar>
           <Button
-            onClick={() => history.push('/')}
+            onClick = {() => history.push("/") }
             size="large"
             edge="start"
             color="inherit"
-            aria-label="home" >
-            Home
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+              Home
           </Button>
           <Button
-            onClick={() => history.push('/movies')}
+            onClick = {() => history.push("/movies") }
             size="large"
             edge="start"
             color="inherit"
-            aria-label="home" >
-            Movies
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          Movies
           </Button>
           <Button
-            onClick={() => history.push('/movies/add')}
+            onClick = {() => history.push("/movies/add-movie") }
             size="large"
             edge="start"
             color="inherit"
-            aria-label="home" >
-            Add Movie
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          Add Movies
           </Button>
           <Button
-            onClick={() => history.push('/color-game')}
+            onClick = {() => history.push("/color-game") }
             size="large"
             edge="start"
             color="inherit"
-            aria-label="home" >
-            Color Game
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          Color Game
           </Button>
-      
+          <Button
+            onClick = {() => history.push("/basic-form") }
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          Basic Form
+          </Button>
+          <Button
+            style= {{marginLeft: "auto"}}
+            onClick = {() => setMode(mode === "light" ? "dark" : "light") }
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            startIcon= {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+          >
+              {(mode === "light" ? "dark" : "light")} Mode
+          </Button>
         </Toolbar>
-        </AppBar>
-      
-      <Switch>
-      <Route exact path="/">
-        <h1>Welcome Movie Lovers</h1>
-        </Route>
-      <Route path="/films">
-          <Redirect to ="/movies"></Redirect>
-        </Route>
-        <Route path="/movies/add">
-        <AddMovie movies={movies} setMovies={setMovies} />
-        </Route>
-        <Route path="/movies/edit/:id">
-          < EditMovie movies={movies}/></Route>
-        <Route path="/movies/:id">
-          < MovieDetails movies={movies}/></Route>
-        <Route path="/movies">
-         <MovieList movies={movies} setMovies={setMovies}/> 
-        </Route>
-        
-        <Route path="/color-game">
-        {<AddColor />} 
-        </Route>
-        <Route path ="**">
-          <h1>Not found</h1>  
-        </Route>
-      </Switch>
-   </div>
+      </AppBar>
+
+<Switch>
+<Route exact path="/">
+<h1 style={{textAlign: 'center', margin: '200px'}}>Welcome Movie Learners</h1>
+</Route>
+
+<Route path="/films">
+  <Redirect to='/movies' />
+</Route>
+
+<Route path="/movies/add-movie">
+  <AddMovie />
+</Route>
+
+<Route path="/movies/edit/:id">
+  <EditMovie />
+</Route>
+
+<Route path="/movies/:id">
+  <MovieDetails />
+</Route>
+
+<Route path="/movies">
+<MovieList />
+</Route>
+
+<Route path="/color-game">
+<AddColor />
+</Route>
+
+<Route path="/basic-form">
+<BasicForm />
+</Route>
+
+<Route path="**">
+<NotFound />
+</Route>
+</Switch>
+
+  </div>
+  </Paper>
+  </ThemeProvider>
   );
 }
-export default App;
 
+function NotFound() {
+  const styles = {width: '100%', objectFit: 'cover'}
+  return (
+    <img style={styles} src='https://www.figmints.com/wp-content/uploads/2019/09/image16.gif' alt="" />
+  ) 
+}
--- src/Counter.js
+++ src/Counter.js
@@ -1,40 +1,36 @@
-import { useState } from "react"
+import { useState } from "react";
 import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AlarmIcon from '@mui/icons-material/Alarm';
 import Badge from '@mui/material/Badge';
-import MailIcon from '@mui/icons-material/Mail';
-function Counter(){
-    const [like,setLike] = useState(0);
-    const [disLike,setDisLike] = useState(0);
-    // const incrementLike = () =>setLike(like+1);
-    // const incrementDisLike = () =>setLike(dislike+1);
-    return(
-      <div className="counter-Container">
-        <IconButton className = "like-dislike"
-        onClick={()=> setLike(like+1)}
-         aria-label="delete"
-         color="primary">
-        
-        <Badge color="primary" badgeContent={like}>👍</Badge>
-      </IconButton>
-      
 
-      <IconButton className = "like-dislike"
-        onClick={()=> setDisLike(disLike+1)}
-         aria-label="delete"
-         color="error">
-         <Badge color="error" badgeContent={disLike}>👎</Badge>
-      </IconButton>
-          </div>
+function Counter() {
+    const [like, setLike] = useState(0)
+    const [disLike, setDisLike] = useState(0)
+    const incrementLike = () => setLike(like + 1)
+    const incrementDisLike = () => setDisLike(disLike + 1)
+    return(
+      <div className="counter-container">
+
+        <IconButton aria-label="like" color='primary' onClick={incrementLike}>
+        <Badge badgeContent={like} color="primary">
+        👍
+        </Badge>
+        </IconButton>
+
+        <IconButton aria-label="dislike" color='primary' onClick={incrementDisLike}>
+        <Badge badgeContent={disLike} color="error">
+        👎
+        </Badge>
+        </IconButton>
+    </div>
     )
   }
+
+  function double(n){
+    return [n, n * 2];
+}
+
+export{Counter,double}
   // 1.named export and import
   //2. default export and import in index.html
 //Only components need to start with  Caps Letter as double is not component it starts with lower case
- function double(n){
-      return [n, n * 2];
-  }
-
-  export{Counter,double}
+ 
